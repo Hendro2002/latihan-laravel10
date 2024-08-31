ad1429b841e1c8540d0edd06bcf533b9d6646ea3
Fauzan <fauzan.xmm2@gmail.com>
2024-08-31 14:30:09 +0700

    collection methods

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,8 +12,8 @@ class StudentController extends Controller
     function index()
     {
         // eloquent
-        // $student = Student::all();
-        // return view('students', ['studentList' => $student]);
+        $student = Student::all();
+        return view('students', ['studentList' => $student]);
         // create
         // Student::create([
         //     'name' => 'coba2',
@@ -27,7 +27,7 @@ class StudentController extends Controller
         //     'class_id' => 3
         // ]);
         // delete
-        Student::find(53)->delete();
+        // Student::find(53)->delete();
 
 
         
@@ -48,7 +48,53 @@ class StudentController extends Controller
         // delete
         // DB::table('students')->where('id', 51)->delete();
 
-        // var_dump('test');
-        // dd($student);
+        $nilai=[1,4,5,3,5,3,4,3,4,3,42,3,4,3,2,1,2,3,4,3,2,23,];
+        // php biasa
+        // // 1. hitung jumlah nilai
+        // $countNilai = count($nilai);
+        // // 2. hitung berapa banyak nilai
+        // $totalNilai = array_sum($nilai);
+        // // 3. hitung rata-rata nilai
+        // $nilaiRataRata = $totalNilai / $countNilai;
+        
+        //collection
+        // $nilaiRataRata = collect($nilai)->avg();
+        
+        // contains
+        // $aaa = collect($nilai)->contains( function ($value){
+        //     return $value == 16;
+        // });
+
+        // diff
+        // $resA=["burger", "siomay","pizza","spageti","makaroni","martabak","bakso"];
+        // $resB=["pizza", "fried chicken","martabak","sayur asem","pecel lele","bakwan","bakso"];
+        // $menuResA = collect($resA)->diff($resB);
+        // $menuResB = collect($resB)->diff($resA);
+
+        // filter
+        // $aaa =collect($nilai)->filter(function($value){
+        //     return $value > 3;
+        // })->all();
+
+        // pluck
+        // $biodata = [
+        //     ['nama' => 'Rizki', 'umur' => 20],
+        //     ['nama' => 'Rahmad', 'umur' => 21],
+        //     ['nama' => 'Rahmawan', 'umur' => 22],
+        //     ['nama' => 'siti', 'umur'=> 16]
+        // ];
+        // $aaa=collect($biodata)->pluck('nama')->all();
+
+        // map
+        // $nilaiKaliDua = [];
+        // foreach ($nilai as $value) {
+        //     array_push($nilaiKaliDua, $value * 2);
+        // }
+
+        // $nilaiKaliDua = collect($nilai)->map(function($value){
+        //     return $value * 2;
+        // })->all();
+
+        // dd($nilaiKaliDua);
     }
 }
